ab970fd8f14c0e5c05d245932d7a27ffec13b2be
Eirik Folkestad <eirik.ek.folkestad@gmail.com>
2017-10-26 17:34:54 +1300

    Finished project

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -8,41 +8,79 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 String input;
 
 void setup() {
+  
   // init Serial
   Serial.begin(115200);
+  
   // define columns and rows of lcd
   lcd.begin(16, 2);
-  // print welcome message
-  lcd.print("Be smart and ->");
+  
+  // print welcome message and scoll to see whole message
+  lcd.print("Be smart and Don't Forget:");
+  delay(1000);
+  for (int i = 0; i < 13; i++) {
+    lcd.scrollDisplayLeft();
+    delay(400);
+  }
+  
   // set LCD cursor to the second row (0,1)
   lcd.setCursor(0,1);
+  
 }
 
 String parseString(String input) {
-    // remove carriage return and newline characters
-    int removeNLastCharacters = input.length()-2;
-    input.remove(removeNLastCharacters);
-    return input;
+  
+    // remove newline character
+    input.remove(0,1);
+
+    // split string on ';'
+    int index = input.indexOf(";");
+    String first = input.substring(0, index);
+    String second = input.substring(index+1);
+
+    // return formated string
+    return first + " & " + second;
+    
 }
 
 void printToLCD(String input) {
+  
     // clear screen and move cursor up to (0,0)
     lcd.clear();
+    
     // print input message
     lcd.print("Don't forget:");
     lcd.setCursor(0,1);
     lcd.print(input);
+    delay(1000);
+    
+    // scroll bottom line while top is "stationary"
+    for (int i = 0; i < input.length()-16; i++) {
+      lcd.scrollDisplayLeft();
+      lcd.setCursor(0,0);
+      for (int j = 0; j < i+1; j++) {
+        lcd.print(" ");
+      }
+      lcd.print("Don't Forget:");
+      delay(400);
+    }
+    
 }
 
 void loop() {
+  
   // check if there are any bytes to be read from the Serial
   while (Serial.available()) {
+    
     // read String from Serial
     input = Serial.readString();
+    
     // print String into Serial
     Serial.println(input);
-    // print String on LCD-screen
+    
+    // print String on LCD-screen formatted
     printToLCD(parseString(input));
+    
     // re-establish connection to Serial to empty cache and buffer
     Serial.begin(115200);
   }
--- D1_mini_v2/D1_mini_v2.ino
+++ D1_mini_v2/D1_mini_v2.ino
@@ -4,75 +4,127 @@
 
 const char* ssid     = "AUT-Sensor";
 const char* password = "AuTS3nS3";
-String apiAUTHENTICATED = "http://api.wunderground.com/api/03f7849bfa25a1b7/hourly/q/NZ/Auckland.json";
-const boolean wifi = false;
-
-const String items[] = {"Rain Jacket", "Sunglasses", "Warm Sweather"};
-int number = 0;
+String apiAUTHENTICATED = "http://api.wunderground.com/api/03f7849bfa25a1b7/forecast/q/NZ/Auckland.json";
+const boolean wifi = true;
 
 void setup() {
+
+  // start serial connection
   Serial.begin(115200);
+
+  // start wifi if we want to
   if (wifi) {
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
-      Serial.println("Connecting..");
+      //Serial.println("Connecting..");
     }
   }
 }
 
 void loop() {
+  
+  // forecast from api values or local values
   if (wifi) {
     wifiEnabled();
   } else {
     wifiDisabled();
   }
-  delay(10000);
+
+  // only get data from api once a minute
+  delay(60000);
+
+  // reset serial connection to clear cache buffer
   Serial.begin(115200);
+  
 }
 
-String forecastItem() {
-  return "Sunglasses";
+void forecastItem(int highTemp, int lowTemp, String condition) {
+  
+    Serial.print("\n");
+
+    // decide item from condition
+    condition.toLowerCase();
+    if (condition.indexOf("rain") > -1) {
+      Serial.print("Rain Jacket");
+    }
+    else if (condition.indexOf("sun") > -1) {
+      Serial.print("Sunglasses");
+    }
+    else if (condition.indexOf("snow") > -1) {
+      Serial.print("Skis");
+    } else {
+      Serial.print("None");
+    }
+
+    // mark item split
+    Serial.print(";");
+
+    // decide item from temperatures
+    if (highTemp >= 20) {
+      Serial.print("T-shirt");
+    }
+    else if (lowTemp < 10) {
+      Serial.print("Warm Jacket");
+    } else {
+      Serial.print("Sweather");
+    }
+    
 }
 
 void wifiEnabled() {
+
+  // check if connected to wifi
   if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected! D1 online...");
+    //Serial.println("Connected! D1 online...");
+
+    // connect to api and get content
     HTTPClient httpClient;
     httpClient.begin(apiAUTHENTICATED);
     int httpReturnValue = httpClient.GET();
-    Serial.println(httpReturnValue);
+
+    // check if anything is being returned
     if (httpReturnValue > 0) {
-      Serial.println("Getting payload...");
+
+      // get data as string
+      //Serial.println("Getting payload...");
       String payload = httpClient.getString();
-      Serial.println(payload);
-      DynamicJsonBuffer jsonBuffer;
-      JsonObject& root = jsonBuffer.parseObject(payload);
+
+      // create jsonbuffer of sufficient size to store json object from string
+      DynamicJsonBuffer jsonBuffer(20000);
+      // create json object from string
+      JsonObject& root = jsonBuffer.parseObject(payload, 50);
+
+      // print if creation of json object fails
       if (!root.success()) {
-        Serial.println("jsonBuffer.parseObject() failed.");
+        Serial.println("jsonBuffer.parseObject() unsuccessful;failed");
       }
-      root.prettyPrintTo(Serial);
+
+      // store wanted values in appropriate variables
+      int highTemp = root["forecast"]["simpleforecast"]["forecastday"][0]["high"]["celsius"];
+      int lowTemp = root["forecast"]["simpleforecast"]["forecastday"][0]["low"]["celsius"];
+      String condition = root["forecast"]["simpleforecast"]["forecastday"][0]["conditions"];
+
+      // forecast item
+      forecastItem(highTemp, lowTemp, condition);
+      
     } else {
-      Serial.println("Something happened --> Could not find API.");
+      // print if api was not found
+      Serial.println("Something happened;Could not find API.");
     }
+    // close http connection
     httpClient.end();
   }
+  
 }
 
 void wifiDisabled() {
   
-  char json[] = "{\"hourly_forecast\":[{\"temp\":{\"english\":\"61\",\"metric\":\"16\"},\"condition\":\"Partly Cloudy\"},{\"temp\":{\"english\":\"59\",\"metric\":\"15\"},\"condition\":\"Partly Cloudy\"}]}";
+  // forecast from static values
+  forecastItem(18,14, "Chance of Rain");
+  //forecastItem(18,14, "Sunny");
+  //forecastItem(20,15, "Cloudy");
   
-  StaticJsonBuffer<1000> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(json);
+}
 
-  if (!root.success())
-  {
-    Serial.println("parseObject() failed");
-    return;
-  }
-  for (int i = 0; i < 2; i++) {
-    root["hourly_forecast"][i]["temp"]["metric"].printTo(Serial);
-    root["hourly_forecast"][i]["condition"].printTo(Serial);
-  }
-  Serial.print("\n");  
+
